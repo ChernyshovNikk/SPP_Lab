5bab66efb4d87ce2a2b4124555ba463db7ff1578
Nikita Chernyshov <NikkOrange96@gmail.com>
2016-09-20 00:36:09 +0300

    1
    First Commit For SPP

---
+++ UnitTest_Lab1/QuickSort_Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab1_SPP;
+
+namespace UnitTest_Lab1
+{
+    [TestClass]
+    public class QuickSort_Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            QuickSort sort = new QuickSort();
+        }
+    }
+}
